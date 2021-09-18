0984b65902364d87bc6c33303a6fa7da358046b2
unknown <Monitoramento@MONITORAMENTO.supremus.seguranca>
2021-09-18 03:51:56 -0300

    Comecando a implementar um metodo que imprime tabuleiro

--- xadrez-console/Tela.cs
+++ xadrez-console/Tela.cs
@@ -9,7 +9,18 @@ namespace xadrez_console
         {
             for (int i=0; i<tab.linhas; i++)
             {
-
+                for (int j=0; j<tab.colunas; j++)
+                {
+                    if (tab.peca(i, j) == null)
+                    {
+                        Console.Write("- ");
+                    }
+                    else
+                    {
+                        Console.Write(tab.peca(i, j) + " ");
+                    }                    
+                }
+                Console.WriteLine();
             }
         }
     }

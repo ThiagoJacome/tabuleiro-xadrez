9227eb866cd982a056aeac9e3373b42ccdfe5dc3
unknown <Monitoramento@MONITORAMENTO.supremus.seguranca>
2021-09-18 03:38:40 -0300

    Comecando a implementar o tabuleiro em outro pc

---
+++ xadrez-console/Tela.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using tabuleiro;
+
+namespace xadrez_console
+{
+    class Tela
+    {
+        public static void imprimirTabuleiro(Tabuleiro tab)
+        {
+            for (int i=0; i<tab.linhas; i++)
+            {
+
+            }
+        }
+    }
+}
